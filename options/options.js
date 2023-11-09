d32362b0fce171131775e93fccbaa9e8ade2fc23
Pen <vspeakman50@gmail.com>
2023-11-09 05:59:08 -0500

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,15 +1,14 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
-  name: "kl-approved-44432",
-  url: "https://kl-approved-44432.botics.co",
-  api: "https://kl-approved-44432.botics.co/api/v1",
-  screenOptions: {
-    headerShown: false
-  }
-}
+  "name": "kl-approved-44432",
+  "url": "https://kl-approved-44432.botics.co",
+  "api": "https://kl-approved-44432.botics.co/api/v1",
+  "screenOptions": {
+    "headerShown": false
+  },
+  "initialRoute": "welcome"
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
